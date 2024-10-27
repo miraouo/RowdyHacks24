75ea673e5b64268369416d3b3055bdc38cd7b6ec
Zach Neill <zacharyneill@gmail.com>
2024-10-27 08:56:55 -0500

    newest qs

--- questions.js
+++ questions.js
@@ -1,9 +1,9 @@
 let questions = [
     {
         numb: 1, 
-        question: "You have access to the Delorean and have absolute free reign! What will you do with you new sick ride?",
+        question: "You have access to the Delorean and absolute free reign! What will you do with your new sick ride?",
         options: [
-            "A. Find a way to recieve all the power and riches of the world, mwahahahaha!",
+            "A. Take over all the power and riches of the world, mwahahahaha!",
             "B. Explore the unknown of what the future holds",
             "C. Partner up and try to the save the world from ending",
             "D. Stick my head out of the window and enjoy the ride"
@@ -11,42 +11,42 @@ let questions = [
     },
     {
         numb: 2, 
-        question: "You've met the love of your life, but she's from the past before you were born, do you take her with you back to your present time?",
+        question: "You've met the love of your life who's from the past before you were born. Do you take your new partner back with you?",
         options: [
-            "A. Of course! Anywhere I'm going, she has no choice but to follow me around heh!",
-            "B. I will never meet a girl like her, I'll stay with her forever!",
+            "A. Of course! Anywhere I'm going, the fella has no choice but to follow me around, heh!",
+            "B. I will never meet someone so special, I'll take my chances!",
             "C. Absolutely not, that would affect the rest of our lives!",
-            "D. I'll go anywhere she goes, she can do no wrong ever in my eyes!"
+            "D. I'll go anywhere my lover goes, who can do no wrong ever in my eyes!"
         ]
     },
     {
         numb: 3, 
         question: "You find yourself 30 years into the future! What souvenier are you bringing back?",
         options: [
-            "A. A copy of sports Almanic, I'm gonna make riches and rule over the world!",
-            "B. Nothing, we can not interfere with the timeline!",
+            "A. A copy of the latest Sports Almanac, I'll make riches and rule the world!",
+            "B. Nothing, we cannot interfere with the timeline!",
             "C. My sick hoverboard, I love to ride in style!",
-            "D. Big stick, yummy bone"
+            "D. A big, really nice stick"
         ]
     },
     {
         numb: 4, 
-        question: "What major time event would you try and change?",
+        question: "What major time event would you try to change?",
         options: [
-            "A. Nothing, I'd travel to everyones worst day and laugh at them",
+            "A. Nothing, I'd travel to everyone's worst day and laugh at them",
             "B. I would want to prevent the burning of the library of Alexandria, there's so much information we don't know",
             "C. Passing a test I failed, shouldn't have stayed up so late playing guitar",
-            "D.  I'd want to go back to see my family so I could hang out with them longer, I love them so much!"
+            "D. I'd want to go back to see my family so I could hang out with them longer!"
         ]
     },
     {
         numb: 5, 
-        question: "It was fun traveling and going on adventures in the Delorean, but now that's it's been destroyed for good, what are you going to do with the limited time you have left on earth?",
+        question: "It was fun adventuring in the Delorean. It's been destroyed for good. What will you do with the time you have left?",
         options: [
             "A. Spend my days yearning for what could have been",
-            "B. Continue my endeavors in enhancing the advancements of technology for the name of science!",
-            "C. Cruise in my sick new truck with my gal and live life to the fullest",
-            "D. Run outside and play fetch with all my friends!"
+            "B. Continue my endeavors in enhancing the advancements of technology in the name of science!",
+            "C. Cruise in my sick new truck with my partner and live life to the fullest",
+            "D. Go outside and hang out with all my friends!"
         ]
     }
 ];
