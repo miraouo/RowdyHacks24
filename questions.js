adbd8e2182633791b812eaa7a393aa56e7f7a548
miraespiritu <mira.espi@gmail.com>
2024-10-26 23:00:47 -0500

    the biggest of all commits ever 43:33

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1, 
+        question: "What does HTML stand for?",
+        answer: "A. option 1",
+        options: [
+            "A. option 1",
+            "B. option 2",
+            "C. option 3",
+            "D. option 4"
+        ]
+    },
+    {
+        numb: 2, 
+        question: "What does HTML stand for?",
+        answer: "A. option 1",
+        options: [
+            "A. option 1",
+            "B. option 2",
+            "C. option 3",
+            "D. option 4"
+        ]
+    },
+    {
+        numb: 3, 
+        question: "What does HTML stand for?",
+        answer: "A. option 1",
+        options: [
+            "A. option 1",
+            "B. option 2",
+            "C. option 3",
+            "D. option 4"
+        ]
+    },
+    {
+        numb: 4, 
+        question: "What does HTML stand for?",
+        answer: "A. option 1",
+        options: [
+            "A. option 1",
+            "B. option 2",
+            "C. option 3",
+            "D. option 4"
+        ]
+    },
+    {
+        numb: 5, 
+        question: "What does HTML stand for?",
+        answer: "A. option 1",
+        options: [
+            "A. option 1",
+            "B. option 2",
+            "C. option 3",
+            "D. option 4"
+        ]
+    }
+];
